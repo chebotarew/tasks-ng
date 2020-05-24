7bb919416104da54b0e3636718e875a3b2fb4979
Alexandr <anchebotarev@dasreda.ru>
2020-05-24 11:04:47 +0300

    module1 test checked

--- 1-components/1-template-syntax-and-binding/src/app/app.component.ts
+++ 1-components/1-template-syntax-and-binding/src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { IProduct, product } from '../mocks/products'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  product: IProduct = product
+  chosenProduct!: IProduct
+
+  addProduct(product: any){
+    this.chosenProduct = product
+  }
+  get ch (){
+    return this.chosenProduct ? JSON.stringify(this.chosenProduct) : ''
+  }
+}
